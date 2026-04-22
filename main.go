a3b50ecca2da91069525667ba2392b35fde50e01
Azamatovich <sizning-emailingiz@gmail.com>
2026-04-22 11:20:51 +0500

    Frontend va Backend birga yangilandi

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 	r.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", gin.H{
-			"Message": "Zar, men seni juda yaxshi ko'raman!",
+			"Message": "Zar seni yaxshi ko'raman!",
 		})
 	})
 
